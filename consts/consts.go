16ce2c18c9849a37c4fd66af451b5798ce846eb0
prathapsingh924 <152711011+prathapsingh924@users.noreply.github.com>
2024-04-07 23:39:38 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "prathap singh"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "PRATHAP"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "PS"
 )
 
 var ID ids.ID
